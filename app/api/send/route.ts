dbbcbd07a6a4532078f18b1dcdd58fdcfb821b20
Aurelien-Valat <auval05@gmail.com>
2025-12-31 13:24:30 +0100

    update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,9 +1,17 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
+
+    const apiKey = process.env.RESEND_API_KEY;
+
+    if (!apiKey) {
+        console.error("La clé API Resend est manquante !");
+        return NextResponse.json({ error: "Missing API Key" }, { status: 500 });
+    }
+
+    const resend = new Resend(apiKey);
+
     try {
         const { name, email, subject, message } = await req.json();
 
